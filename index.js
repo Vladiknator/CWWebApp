1d5046762ce30d2d5e45a6b35f223ef00b12a66a
Vladislav Pyatkin <vladiknator@gmail.com>
2024-02-29 18:41:19 -0700

    fix spelling error

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(
 
 async function doSQL(statement, params) {
   const client = new pg.Client({
-    host: process.env.DBID,
+    host: process.env.DBIP,
     port: 5432,
     database: 'creativewriting',
     user: process.env.DBUSER,
